7b0e70f0f5aa216dbe6b4123b2d3524352318c46
Pau111111 <paugarciapellicer96@gmail.com>
2021-06-01 15:44:24 +0200

    Company model created

--- djangoProject/djangoAPI/models.py
+++ djangoProject/djangoAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+    website = models.CharField(max_length=100)
