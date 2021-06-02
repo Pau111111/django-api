ed2ca244d116031e72bb51297a38d9bcb57f74c6
Pau111111 <paugarciapellicer96@gmail.com>
2021-06-02 10:04:30 +0200

    Custom class implemented

---
+++ djangoProject/djangoAPI/src/MyClass.py
@@ -0,0 +1,9 @@
+class CustomClass:
+    def __init__(self):
+        num1 = 1
+        num2 = 1
+        return num1+num2
+    def myMethod():
+        num1 = 2
+        num2 = 2
+        return num1+num2
--- djangoProject/djangoAPI/views.py
+++ djangoProject/djangoAPI/views.py
@@ -1,8 +1,10 @@
+import json
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 from .models import Company
 from django.forms.models import model_to_dict
+from .src.MyClass import CustomClass
 
 # Create your views here.
 class CompanyListView(View):
@@ -25,7 +27,12 @@ class CompanyListView(View):
 class CompanyDetailView(View):
     def get(self, request, pk):
         company = Company.objects.get(pk=pk)
+        myClass = CustomClass.myMethod()
+        jsonVar = {"data": 100}
+        print("Holaaaa" + str(jsonVar["data"]))
+
         #El segundo parámetro "False" indica que no se va a devolver un objeto JSON, sinó un Array de objetos JSON
+        return JsonResponse(jsonVar, safe=False)
         return JsonResponse(model_to_dict(company))
     # def post(self, request):
     #     # post...
